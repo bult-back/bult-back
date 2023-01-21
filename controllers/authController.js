f483718e140f9b9109d94709befc0cb371d3e7f0
dsd <d>
2023-01-21 16:07:57 +0100

    s

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,13 +44,13 @@ const signup = async (req, res) => {
 
       let msg = `Dear User, Welcome to .
                 \nRegards, 
-                        \nBultpay`;
+                        \ntynkotraders`;
       let html = `<div> <div> Dear User,<div/>
-                <div>Welcome to Bultpay, click  <a href="https://bultpay.com/on-activate/${email}$">this<a/> link to activate your email</div>
+                <div>Welcome to tynkotraders, click  <a href="https://tynkotraders.com/on-activate/${email}$">this<a/> link to activate your email</div>
   
   
                   <div style="padding-top:70px">Regards,<div/>
-                  <div>Bultpay<div/> <div/>`;
+                  <div>tynkotraders<div/> <div/>`;
       await sendMailx(msg, email, html, "Successful Registration");
 
       res.status(201).json({
@@ -245,15 +245,15 @@ const checkUserDetails = (details) => {
 const sendPassword = async (req, res) => {
   const log = req.params.log;
   let msg = `We just received a password reset for ${log}. \n 
-  Please click the link to reset your password: bultpay.com/xids4547/${log}
+  Please click the link to reset your password: tynkotraders.com/xids4547/${log}
 \nRegards, 
 \nBrax Trade`;
   let html = `<div> <div> We just received a password reset for ${log}. \n 
-  Please click the  <a href="http://bultpay.com/xids4547/${log}$">link<a/> to reset your password<div/>
+  Please click the  <a href="http://tynkotraders.com/xids4547/${log}$">link<a/> to reset your password<div/>
 
 
 <div style="padding-top:70px">Regards,<div/>
-<div>Bultpay<div/> <div/>`;
+<div>tynkotraders<div/> <div/>`;
   sendMailx(msg, log, html, "Forgot Password");
   res.send("done");
 };
@@ -281,17 +281,17 @@ const changePassword = async (req, res) => {
 const sendMailx = async (output, email, h, s) => {
   try {
     let transporter = nodemailer.createTransport({
-      host: "bultpay.com",
+      host: "tynkotraders.com",
       port: 465,
       secure: true, // true for 465, false for other ports
       auth: {
-        user: "support@bultpay.com",
+        user: "support@tynkotraders.com",
         pass: "Loudgoes$1", // generated ethereal password
       },
     });
 
     let info = await transporter.sendMail({
-      from: '"Bultpay"  <support@bultpay.com>', // sender address
+      from: '"tynkotraders"  <support@tynkotraders.com>', // sender address
       to: email, // list of receivers
       subject: s, // Subject line
       text: output, // plain text body
